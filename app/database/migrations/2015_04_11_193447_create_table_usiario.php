2cb8b1952ddb14f6610cd3eadbdce2c6fd7af97e
Andres Bedoya <Andres Bedoya@172.32.22.108>
2015-04-11 18:08:46 -0500

    Clase 5

---
+++ app/database/migrations/2015_04_11_193447_create_table_usiario.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsiario extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('usuario', function(Blueprint $table)
+		{
+                    $table->increments('id');
+                    $table->string('nombre');
+                    $table->string('correo');
+                    $table->string('password');
+                    $table->rememberToken();
+                    $table->timestamps();
+		});
+                
+                Schema::create('publicacion', function(Blueprint $table)
+		{
+                    $table->increments('id');
+                    $table->timestamps();
+                    $table->text('publicacion');
+                    $table->boolean('tipo');
+                    $table->integer('usuario_id')->unsigned();
+                    $table->foreign('usuario_id')->references('id')->on('usuario');
+                    $table->integer('padre')->unsigned()->nullable;
+                    $table->foreign('padre')->references('id')->on('publicacion');
+		});
+                
+                Schema::create('me_gusta', function(Blueprint $table)
+		{
+                    $table->increments('id');
+                    $table->integer('usuario_id')->unsigned();
+                    $table->foreign('usuario_id')->references('id')->on('usuario');
+                    $table->integer('publicacion_id')->unsigned();
+                    $table->foreign('publicacion_id')->references('id')->on('publicacion');
+                    $table->timestamps();
+		});
+                
+                DB::table('usuario')
+                        ->insert([
+                            'nombre'=>'Andres',
+                            'correo'=>'jabedoyah@gmail.com',
+                            'password'=>Hash::make('12345')
+                            
+                        ]);
+                DB::table('usuario')
+                        ->insert([
+                            'nombre'=>'Luis',
+                            'correo'=>'luis@gmail.com',
+                            'password'=>Hash::make('12345')
+                            
+                        ]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('usuario', function(Blueprint $table)
+		{
+                    Schema::drop('me_gusta');
+                    Schema::drop('publicacion');
+                    Schema::drop('usuario');
+		});
+	}
+
+}
--- app/routes.php
+++ app/routes.php
@@ -11,16 +11,42 @@ Route::get('/prueba', function()
 });
 
 
-Route::get('/jabedoyah', function()
+Route::get('/profile', array('before' => 'auth', function()
 {
 	return View::make('perfil.perfil')
-                ->with("nombre", "Andres");
+                ->with("nombre", Auth::user()->nombre);
 
+}));
+
+Route::get('/', function()
+{
+        if(Auth::check()){
+            return Redirect::to("/profile");
+        }
+	return View::make('general.login');
 });
 
 Route::get('/calendario', function()
 {
 	return View::make('calendario');
+});
+
+Route::post('/login', function()
+{
+	$email = Input::get('correo');
+        $password = Input::get('password');
+        if (Auth::attempt(array('correo' => $email, 'password' => $password)))
+        {
+            return Redirect::to("/profile");
+        }else{
+            echo "No está logueado";
+        }
+});
+
+Route::get('/logout', function()
+{
+	Auth::logout();
+        return Redirect::to("/");
 });
 
 
